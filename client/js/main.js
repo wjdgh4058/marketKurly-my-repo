e2b82687aca7a7570bfb19730d5ad9da0671027d
leejungho <ljhmoa@gmail.com>
2023-02-02 17:49:51 +0900

    [Update] loop, autoplay 설정 완료

--- client/js/main.js
+++ client/js/main.js
@@ -66,3 +66,21 @@ var toggleMainPopup = function () {
 $(function () {
   toggleMainPopup();
 });
+
+// swiper
+let mainBannerSwiper = new Swiper('.Swiper1', {
+  loop: true, // 무한 반복
+  // autoplay 3초마다 넘기기
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
