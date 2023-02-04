f6cf816c8dc87e583408fa9e2125408f59d3aa4d
leejungho <ljhmoa@gmail.com>
2023-02-04 21:53:57 +0900

    [Update] 헤더푸터 추가

--- client/js/popup.js
+++ client/js/popup.js
@@ -1,4 +1,4 @@
-import { getNode } from '../lib/index.js';
+// import { getNode } from '../lib/index.js';
 
 // 팝업창 제어
 var toggleMainPopup = function () {
@@ -66,30 +66,4 @@ var toggleMainPopup = function () {
 
 $(function () {
   toggleMainPopup();
-});
-
-// main banner swiper1
-let mainBannerSwiper = new Swiper('.swiper1', {
-  loop: true, // 무한 반복
-  // autoplay 3초마다 넘기기
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-// mousehover stop start 설정
-$('.swiper-slide').on('mouseover', function () {
-  mainBannerSwiper.autoplay.stop();
-});
-$('.swiper-slide').on('mouseout', function () {
-  mainBannerSwiper.autoplay.start();
 });
